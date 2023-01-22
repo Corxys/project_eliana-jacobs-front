44b1b965d45dfa25c3db5bbb47973a96a1d5dd2d
jlboulanger <jennylee.boulanger@caldera.com>
2023-01-22 13:28:25 +0100

    non imported files

---
+++ src/components/project-components/audio-player-component.vue
@@ -0,0 +1,18 @@
+<script setup>
+// Props
+defineProps({
+	"media": {
+		"type": Object,
+		"required": true,
+	},
+});
+</script>
+
+<template>
+	<div class="interview-page__audio__file">
+		<div class="interview-page__audio__file__source">
+			<audio :id="media.id" :src="media.src.data.attributes.url" controls>
+			</audio>
+		</div>
+	</div>
+</template>
