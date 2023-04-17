ed21f7dcd86e83ed8caa8a0cdec4450f6e774eee
jlboulanger <jennylee.boulanger@caldera.com>
2023-04-17 19:14:12 +0200

    test for router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "../views/home-view.vue";
+import {defineAsyncComponent} from "vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,37 +16,37 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/about-view.vue"),
+      component: defineAsyncComponent(() => import("../views/about-view.vue")),
     },
     {
       path: "/article/:slug",
       name: "article",
-      component: () => import("../views/article-view.vue"),
+      component: defineAsyncComponent(() => import("../views/article-view.vue")),
     },
     {
       path: "/contact",
       name: "contact",
-      component: () => import("../views/contact-view.vue"),
+      component: defineAsyncComponent(() => import("../views/contact-view.vue")),
     },
     {
       path: "/legal-notices",
       name: "legal-notices",
-      component: () => import("../views/legal-notices-view.vue"),
+      component: defineAsyncComponent(() => import("../views/legal-notices-view.vue")),
     },
     {
       path: "/news",
       name: "news",
-      component: () => import("../views/news-view.vue"),
+      component: defineAsyncComponent(() => import("../views/news-view.vue")),
     },
     {
       path: "/projects/:slug",
       name: "projects",
-      component: () => import("../views/projects-view.vue"),
+      component: defineAsyncComponent(() => import("../views/projects-view.vue")),
     },
     {
       path: "/projects/:slug/:slug",
       name: "project",
-      component: () => import("../views/project-view.vue"),
+      component: defineAsyncComponent(() => import("../views/project-view.vue")),
     },
   ],
 });
