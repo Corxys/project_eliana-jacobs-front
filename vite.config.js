1ed04beb0df985923b417b594347be11843363a4
jlboulanger <jennylee.boulanger@caldera.com>
2023-07-10 21:35:36 +0200

    conf: chunk size limit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  "build": {
+    "chunkSizeWarningLimit": 2000,
+  },
   "plugins": [vue()],
   "resolve": {
     "alias": {
